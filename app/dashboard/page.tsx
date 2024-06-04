cf5893ef40c3d79f9da3ad0f96b0b3dd646b25c1
XIANMIAO\86173 <linxm@broda.com>
2024-06-04 22:00:07 +0800

    course

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,40 @@
-const Page = () => {
-    return <p>Dashboard Page</p>;
+import { Card } from '@/app/ui/dashboard/cards';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+
+//mockData
+import { revenue } from '../lib/placeholder-data';
+const invoices = [
+    {
+        id: '3958dc9e-712f-4377-85e9-fec4b6a6442a',
+        name: 'Delba de Oliveira',
+        email: 'delba@oliveira.com',
+        image_url: '/customers/delba-de-oliveira.png',
+        amount: '15795'
+    }
+];
+const numberOfInvoices = 20;
+const numberOfCustomers = 20;
+const totalPaidInvoices = 20;
+const totalPendingInvoices = 20;
+
+export default async function Page() {
+    return (
+        <main>
+            <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+                Dashboard
+            </h1>
+            <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+                <Card title="Collected" value={totalPaidInvoices} type="collected" />
+                <Card title="Pending" value={totalPendingInvoices} type="pending" />
+                <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+                <Card title="Total Customers" value={numberOfCustomers} type="customers" />
+            </div>
+            <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+                <RevenueChart revenue={revenue} />
+                <LatestInvoices latestInvoices={invoices} />
+            </div>
+        </main>
+    );
 }
-export  default Page;
--- app/lib/placeholder-data.js
+++ app/lib/placeholder-data.js
@@ -72,6 +72,7 @@ const customers = [
   },
 ];
 
+
 const invoices = [
   {
     customer_id: customers[0].id,
